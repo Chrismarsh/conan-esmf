1ed20b3d15b89b2eb757ba45980d83e16a43a4a1
Chris Marsh <chrismarsh.c2@gmail.com>
2021-03-15 11:51:11 -0600

    Per OS detection and packaging

--- build.py
+++ build.py
@@ -3,27 +3,12 @@ from collections import defaultdict
 from sys import platform
 
 if __name__ == "__main__":
-
-    command=""
-    if platform == "linux":
-        command = "sudo apt-get -qq update && sudo apt-get -qq install gfortran"
-
     builder = ConanMultiPackager(cppstds=[14],
                                 archs=["x86_64"],
-                                build_types=["Release"],
-                                docker_entry_script = command)
-                              
+                                build_types=["Release"])
+                        
     builder.add_common_builds(pure_c=False, shared_option_name=False)
+    builder.remove_build_if(lambda build: build.settings["compiler.libcxx"] == "libstdc++") # old ABI
 
-    builder.remove_build_if(lambda build: build.settings["compiler.libcxx"] == "libstdc++")
-
-
-    named_builds = defaultdict(list)
-    for settings, options, env_vars, build_requires, reference in builder.items:
-
-        named_builds[settings['compiler']].append([settings, options, env_vars, build_requires, reference])
-
-    builder.named_builds = named_builds
 
     builder.run()
-
